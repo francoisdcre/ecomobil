b1f73ab5668457ebf1fc9697d48f79034d8a94ea
francoisdcre <francois.dcre@gmail.com>
2025-03-31 13:58:51 +0200

    Update BDD Connection

--- app/models/BddModel.php
+++ app/models/BddModel.php
@@ -6,15 +6,18 @@ class BddModel {
 
     // Paramètres de connexion
 
-    private $host = 'localhost';
-    private $dbname = 'ecomobil';
-    private $username = 'root';
-    private $password = '';
+    private $env;
+    private $host = 'localhost:3306';
+    private $dbname = 'francois_ecomobil';
+    private $username = 'francois_bdd';
+    private $password;
     private $conn;
 
     // Méthode pour la connexion à la base de données
 
     public function getConnection() {
+        $this->env = parse_ini_file(__DIR__ . '/../../.env');
+        $this->password = $this->env['ECOMOBIL_BDD'];
         $this->conn = null;
         try {
             $this->conn = new PDO('mysql:host=' . $this->host . '; dbname=' . $this->dbname . '; charset=utf8', $this->username, $this->password, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
